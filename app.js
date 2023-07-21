e03668740bdba9ab9c95a653c72463491b702718
harundmrpnce <harundemirpence@gmail.com>
2023-07-21 19:07:04 +0300

    remake

---
+++ app.js
@@ -0,0 +1,145 @@
+let index = 0;
+const lessons = document.querySelector(".lesson-list");
+const addButton = document.querySelector(".add");
+
+addButton.addEventListener("click", updateHtml);
+addButton.addEventListener("click", calculateAverage);
+lessons.addEventListener("change", calculateAverage);
+document.querySelector(".credit").addEventListener("input", calculateAverage);
+document.querySelector(".average").addEventListener("input", calculateAverage);
+
+function updateHtml() {
+    const lessonDiv = document.createElement("div");
+    lessonDiv.classList.add("lesson");
+    lessons.append(lessonDiv);
+    
+    const lessonName = document.createElement("input");
+    lessonName.type = "text";   
+    lessonName.name = "lesson-name-" + index;
+    lessonName.placeholder = "Ders İsmi"
+    lessonName.classList.add("input", "lesson-item");
+
+    const lessonGrade = document.createElement("select");
+    lessonGrade.name = "lesson-grade-" + index;
+    lessonGrade.classList.add("select", "lesson-item", "lesson-grade");
+    lessonGrade.innerHTML = `
+        <option value="0">Not</option>
+        <option value="4">AA</option>
+        <option value="3.5">BA</option>
+        <option value="3">BB</option>
+        <option value="2.5">CB</option>
+        <option value="2">CC</option>
+        <option value="1.5">DC</option>
+        <option value="1">DD</option>
+        <option value="0">FF</option>
+    `;
+    
+    const lessonCredit = document.createElement("select");
+    lessonCredit.name = "lesson-credit-" + index;
+    lessonCredit.classList.add("select", "lesson-item", "lesson-credit");
+    lessonCredit.innerHTML = `
+        <option value="0">Kredi</option>
+        <option value="1">1</option>
+        <option value="1.5">1.5</option>
+        <option value="2">2</option>
+        <option value="2.5">2.5</option>
+        <option value="3">3</option>
+        <option value="3.5">3.5</option>
+        <option value="4">4</option>
+        <option value="4.5">4.5</option>
+        <option value="5">5</option>
+    `;
+
+    const removeButton = document.createElement("span");
+    removeButton.innerText = "delete";
+    removeButton.classList.add("material-symbols-outlined", "icon", "lesson-item");
+    removeButton.addEventListener("click", function() {
+        lessonDiv.remove();
+        calculateAverage();
+    });
+
+    const retakeLabel = document.createElement("label");
+    retakeLabel.innerText = "Tekrar?"
+    retakeLabel.htmlFor = "retake-" + index;
+    retakeLabel.classList.add("lesson-item", "retake-label");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("lesson-item", "checkbox");
+    checkbox.id = "retake-" + index;
+    
+    const retakeGrade = document.createElement("select");
+    retakeGrade.classList.add("lesson-item", "select", "retake-grade");
+    retakeGrade.disabled = true;
+
+    checkbox.addEventListener("change", function() {
+        calculateAverage();
+        if(checkbox.checked) {
+            retakeGrade.disabled = false;
+        } else {
+            retakeGrade.disabled = true;
+        }
+    })
+
+    retakeGrade.innerHTML = `
+        <option value="0">Eski Not</option>
+        <option value="4">AA</option>
+        <option value="3.5">BA</option>
+        <option value="3">BB</option>
+        <option value="2.5">CB</option>
+        <option value="2">CC</option>
+        <option value="1.5">DC</option>
+        <option value="1">DD</option>
+        <option value="0">FF</option>
+    `;
+
+    lessonDiv.append(lessonName, lessonGrade, lessonCredit, retakeLabel, checkbox, retakeGrade, removeButton);
+    index++;
+    calculateAverage();
+}
+
+function calculateAverage() {
+    const creditInput = document.querySelector(".credit");
+    const averageInput = document.querySelector(".average");
+    let previousCredit = 0;
+    let previousAverage = 0;
+    if (creditInput.value === "") {
+        previousCredit = 0;
+    } else {
+        previousCredit = parseFloat(creditInput.value);
+    }
+
+    if (averageInput.value === "") {
+        previousAverage = 0;
+    } else {
+        previousAverage = parseFloat(averageInput.value);
+    }
+    
+    let creditSum = previousCredit;
+    let gradeSum = previousAverage * previousCredit;
+    let average = 0
+
+    const lessonList = document.querySelectorAll(".lesson");
+
+    for (let i = 0; i < lessonList.length; i++) {
+        const lessonGrade = parseFloat(lessonList[i].querySelector(".lesson-grade").value);
+        const lessonCredit = parseFloat(lessonList[i].querySelector(".lesson-credit").value);
+        const checkbox = lessonList[i].querySelector(".checkbox");
+
+        if (checkbox.checked) {
+            const retakeGrade = parseFloat(lessonList[i].querySelector(".retake-grade").value);
+            creditSum -= lessonCredit;
+            gradeSum -= retakeGrade * lessonCredit;
+        }
+
+        gradeSum += lessonGrade * lessonCredit;
+        creditSum += lessonCredit;
+    }
+
+    if (creditSum > 0) {
+        average = gradeSum / creditSum;
+    }
+
+    const result = document.querySelector("#result");
+    result.innerText = average.toFixed(2);
+}
